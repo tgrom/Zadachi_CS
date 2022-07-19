af8ad0375ae5e128123954aaf9fece9981c5e0f6
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 12:45:41 +0300

    Восьмая задача

---
+++ HW008/Program.cs
@@ -0,0 +1,21 @@
+﻿/*int b = -20;
+int a = 20;
+//Console.WriteLine(a);
+int i = b;
+while (i <= a)
+{
+    Console.Write($"{i} ");
+
+    i = i + 1;
+}
+*/
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int b = -n;
+int i = b;
+while (i <= n)
+{
+    Console.Write($"{i} ");
+
+    i = i + 1;
+}
