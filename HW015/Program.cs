0f241a2e469591ce64422bed3ec3ef280a47b3e6
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 16:48:13 +0300

    Пятнадцатая задача

---
+++ HW015/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100)
+{
+    Console.WriteLine("Нет третьей цифры у данного числа");
+}
+else if (n >= 100 && n < 1000)
+{
+    Console.WriteLine(n % 10);
+}
+else if (n >= 1000 && n < 10000)
+{
+    int a = n / 10;
+    Console.WriteLine(a % 10);
+}
+else if (n >= 10000 && n < 100000)
+{
+    int a = n / 100;
+    Console.WriteLine(a % 10);
+}
+else Console.WriteLine("Необходимо ввести число меньше 100000");
+
+
