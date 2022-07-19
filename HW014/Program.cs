de383c14e1417efcfb59d81cf01a32f7b514bbc0
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 15:44:44 +0300

    Четырнадцатая задача

---
+++ HW014/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b % a == 0)
+{
+    Console.WriteLine("Число a кратно числу b");
+}
+else Console.WriteLine(a % b);
