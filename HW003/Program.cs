11057257c7876f416e8185fbfb870b60a63c948c
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 10:26:15 +0300

    Третья задача

---
+++ HW003/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else if (b > a)
+{
+    Console.WriteLine(b);
+}
+else
+
+    Console.WriteLine("Числа равны");
