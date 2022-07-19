a561cf2b43fbe6bbea6a40394e3c7d77e0704ab2
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 14:38:44 +0300

    Одиннадцатая задача

---
+++ HW011/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 10)
+{
+    Console.WriteLine("Необходимо ввести число больше 9");
+}
+else if (n >= 10 && n < 100)
+{
+    Console.WriteLine(n / 10);
+}
+else if (n >= 100 && n < 1000)
+{
+    int a = n / 10;
+    int b = a % 10;
+    Console.WriteLine(b);
+}
+else Console.WriteLine("Необходимо ввести число меньше 1000");
