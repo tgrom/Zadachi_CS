5268a78312cec725d4b2b9a9166dfb08cc7642a6
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 14:42:59 +0300

    Двенадцатая задача

---
+++ HW012/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a = n / 10;
+int b = n % 10;
+
+if (a >= b)
+{
+    Console.WriteLine(a);
+}
+else Console.WriteLine(b);
