c842fca514923b8fec6a1a5c392ca5444ccf29a9
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 10:03:46 +0300

    Вторая задача

---
+++ HW002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("Да");
+}
+if (b == a * a)
+{
+    Console.WriteLine("Да");
+}
+else Console.WriteLine("Нет");
