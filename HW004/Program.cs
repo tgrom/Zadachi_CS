2796caf18561143a50f9373cd64ef8a733384c21
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-19 11:05:07 +0300

    Четвертая задача

---
+++ HW004/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число, соответсующие дню недели");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 1 && a <= 7)
+{
+    if (a == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    if (a == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    if (a == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    if (a == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    if (a == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    if (a == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    if (a == 7)
+    {
+        Console.WriteLine("Воскресенте");
+    }
+}
+else Console.WriteLine("Не соответсвует дню недели");
