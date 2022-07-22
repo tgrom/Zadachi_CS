0555603598c0c45a2f29a9e35f19e03d3008de95
tgrom <tg.romanchenko@gmail.com>
2022-07-22 22:28:46 +0300

    Семнадцатая задача

---
+++ HW017/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число от 0 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+bool b = true;
+if (1 <= a && a <= 7) {
+
+if (a == 6 || a == 7) 
+{
+    System.Console.WriteLine(b);
+}
+else System.Console.WriteLine(!b);
+}
+else System.Console.WriteLine("Введите правильное число");
