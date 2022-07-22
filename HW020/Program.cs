c6438b22d5ebc5de869079f07cedfa88a84fecbc
tgrom <tg.romanchenko@gmail.com>
2022-07-23 00:02:27 +0300

    Двадцатая задача

---
+++ HW020/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите число X");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("I");;
+}
+if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("II");;
+}
+if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("III");;
+}
+if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("IV");;
+}
