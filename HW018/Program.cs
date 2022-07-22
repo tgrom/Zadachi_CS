9d44454abc89305edbc79392d15ac4587563db37
tgrom <tg.romanchenko@gmail.com>
2022-07-22 22:51:48 +0300

    18_1

--- HW018/Program.cs
+++ HW018/Program.cs
@@ -2,13 +2,12 @@
 int a = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите число ");
 int b = Convert.ToInt32(Console.ReadLine());
-bool c = true;
 
-if (a == b*b || b == a*a)
+
+
+System.Console.WriteLine(Chek_out(a, b));
+
+bool Chek_out(int a, int b)
 {
-    System.Console.WriteLine(c);
-}
-else
-{
-    System.Console.WriteLine(!c);
+    return a == b * b || b == a * a;
 }
