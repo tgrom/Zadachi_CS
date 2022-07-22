05d4dac16c7b167f977b6507d834c60f96572d64
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-22 15:31:25 +0300

    Шестнадцатая задача

---
+++ HW016/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("YES");
+}
+else Console.WriteLine("NO");
