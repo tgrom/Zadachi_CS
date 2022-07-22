8f3abf3ff62d5e8399886dd4255025433b97219c
tgrom <tg.romanchenko@gmail.com>
2022-07-22 22:35:55 +0300

    Восемнадцатая задача

---
+++ HW018/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool c = true;
+
+if (a == b*b || b == a*a)
+{
+    System.Console.WriteLine(c);
+}
+else
+{
+    System.Console.WriteLine(!c);
+}
