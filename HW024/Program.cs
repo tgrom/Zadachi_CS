a6b3aac8b1877479d45b973a74add0f94a61c34a
tgrom <tg.romanchenko@gmail.com>
2022-07-24 17:22:49 +0300

    Двадцать четвертая задача

---
+++ HW024/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= n)
+{
+    System.Console.Write($"{i} ");
+    System.Console.WriteLine(i * i);
+    i= i + 1;
+}
