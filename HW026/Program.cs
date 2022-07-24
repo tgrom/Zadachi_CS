f5e85a318f40bc7bbfb71e75c766b41561e993cd
tgrom <tg.romanchenko@gmail.com>
2022-07-24 18:20:12 +0300

    Двадцать шестая задача

---
+++ HW026/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int sum = 0;
+ 
+
+
+while (i <= n)
+{
+    sum = sum + i;
+    i = i + 1;
+}
+System.Console.WriteLine(sum);
