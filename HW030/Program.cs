45f6bf51fccc4ecfad1e78387c58c8e7cbc05c2d
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-25 14:10:36 +0300

    Тридцатая задача

---
+++ HW030/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int mult = 1;
+
+int Multiplication(int n)
+{
+    while (i <= n)
+    {
+        mult = mult * i;
+        i++;
+    }
+    return mult;
+}
+
+System.Console.WriteLine(Multiplication(n));
+
+
+
