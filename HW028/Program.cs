e681f9db604df40baa401f425406bb2ae5275bc3
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-25 11:57:50 +0300

    Двадцать восьмая задача

---
+++ HW028/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+
+
+
+int CountDig(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            i++;
+            n /= 10;
+        }
+        return i;
+    }
+}
+System.Console.WriteLine(CountDig(n));
