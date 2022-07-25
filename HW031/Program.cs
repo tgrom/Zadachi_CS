ed67c3b8396c4b4bce847a8a9c4f4be4f4221dbe
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-25 15:14:48 +0300

    Тридцать первая задача

---
+++ HW031/Program.cs
@@ -0,0 +1,41 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+//int cub = i * i * i;
+//int cub;
+
+//System.Console.Write("Число |");
+//System.Console.WriteLine("Куб числа");
+
+
+
+while (i <= n)
+{
+    int cub = i * i * i;
+    if (cub % 2 == 0)
+
+        System.Console.WriteLine(cub);
+
+    i = i + 1;
+}
+
+
+
+
+
+
+
+
+/*int Even(int n)
+{
+    while (i <= n)
+    {
+        return cub;
+
+        i = i + 1;
+    }
+
+}
+System.Console.WriteLine(Even(n));
+*/
