e6f575738d02c72bbb7ef787255e29174883ebda
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-25 11:16:30 +0300

    Двадцать седьмая задача

---
+++ HW027/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int pow = 1;
+int i = 1;
+
+while (i <= b)
+{
+    pow = pow * a;
+    i = i + 1;
+}
+System.Console.WriteLine(pow);
+
+
