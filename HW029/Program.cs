db2edd4b7e42e023501d62788f35c07a1e078d3a
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-25 12:42:50 +0300

    Двадцать девятая задача

---
+++ HW029/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+//int i = 0;
+//int k;
+int sum = 0;
+
+System.Console.WriteLine(SumDig(n));
+
+
+
+
+
+
+
+int SumDig(int n)
+{
+    if (n < 10)
+    {
+        return n;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            int k = n % 10;
+            sum = sum + k;
+
+            n /= 10;
+
+        }
+        return sum;
+    }
+}
