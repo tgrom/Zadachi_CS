2a1e42f6dbb808cffc7c0827524ae8ff56fb7f0b
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-27 17:20:54 +0300

    Тридцать Девятая задача

---
+++ HW039/Program.cs
@@ -0,0 +1,28 @@
+﻿//Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int N = 7;
+int[] a = new int[N];
+Random random = new Random();
+
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(1, 20);
+
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    System.Console.Write($"{a[i],6}");
+
+}
+for (int i = 1; i < a.Length; i = i + 2)
+
+    System.Console.Write($"\nЧисло с нечетным индексом: {a[i],4}");
+for (int i = 1; i < a.Length; i = i + 2)
+    sum += a[i];
+System.Console.WriteLine($"\nСумма чисел нечетных индексов массива = {sum}");
+
+
+
+
