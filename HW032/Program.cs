5eec007148782b841a54364ef4beb6848703c314
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-27 14:53:12 +0300

    Тридцать вторая задача

---
+++ HW032/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать массив из 8 целых элементов и вывести их на экран
+
+int N = 8;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(1, 20);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
+
+
