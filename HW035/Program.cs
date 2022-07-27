1472b9ac2b1c80ae28516e6f68d4bbe6289dae52
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-27 16:18:54 +0300

    Тридцать пятая задача

---
+++ HW035/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу замены элементов массива на противоположные
+
+int N = 4;
+int[] a = new int[N];
+int[] b = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(-9, 10);
+
+    b[i] = -1 * a[i];
+
+}
+
+for (int i = 0; i < a.Length; i++)
+
+    System.Console.Write($"{a[i],4}");
+System.Console.WriteLine();
+
+
+
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{b[i],4}");
+
+
+
+
+
+
+
+
+
+
+
+
+
