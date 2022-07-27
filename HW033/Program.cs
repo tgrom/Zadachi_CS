091721ac8b87bd0164481789acfb1d0e7f3be995
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-27 15:06:43 +0300

    Тридцать третья задача

---
+++ HW033/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int N = 8;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 2);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
