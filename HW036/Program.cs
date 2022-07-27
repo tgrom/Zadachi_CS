ba2940b65bd058a380f6f77577807c41f079c0ed
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-07-27 16:30:26 +0300

    Тридцать Шестая задача

---
+++ HW036/Program.cs
@@ -0,0 +1,19 @@
+﻿//Определить, присутствует ли в заданном массиве, некоторое число
+
+int N = 5;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(1, 5);
+
+int number = 5;
+int j = 0;
+while (j < a.Length && a[j] != number) j++;
+if (j == a.Length)
+{
+    System.Console.WriteLine("NO");
+}
+else System.Console.WriteLine("Yes");
+
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
