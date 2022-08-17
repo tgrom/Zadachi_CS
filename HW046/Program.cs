9ba815ed099ceb27087c3af19cd08661a77df7b6
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-08-17 15:37:40 +0300

    Сорок шестая задача

---
+++ HW046/Program.cs
@@ -0,0 +1,36 @@
+﻿System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+Fibonacci();
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Fibonacci()
+{
+
+    int f1 = 0, f2 = 1, i;
+
+    if (n < 1)
+        return;
+    Console.Write(f1 + " ");
+
+    for (i = 1; i < n; i++)
+    {
+        Console.Write(f2 + " ");
+        int next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+    }
+}
