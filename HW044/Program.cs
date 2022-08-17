892db09b300ac2328a78c50abf37c50cd68f32d4
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-08-17 12:23:09 +0300

    Сорок Четвертая задача

---
+++ HW044/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+Main();
+
+void Main()
+{
+    string binary = Convert.ToString(a, 2);
+    Console.WriteLine(binary);
+
+}
