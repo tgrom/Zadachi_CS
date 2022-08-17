43a9ebe7c842a43fd70a4da5d14e4f097726c3c5
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-08-17 14:04:34 +0300

    Сорок Пятая задача

---
+++ HW045/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Введите число");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+double b2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+Crossroad();
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Crossroad()
+{
+
+    double x = ((b2 - b1) * 1.0) / ((k1 - k2) * 1.0);
+    double y = k1 * x + b1;
+    System.Console.WriteLine($"({x}; {y})");
+}
+
+
+
