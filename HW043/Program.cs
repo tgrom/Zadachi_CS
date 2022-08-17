7130fd849c7f0694aebf04feacdc2f067f04a27d
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-08-17 11:53:56 +0300

    Сорок Третья задача

---
+++ HW043/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+Console.ReadLine();
+string s = Console.ReadLine();
+string[] ss = s.Split(' ');
+int[] a = Array.ConvertAll<string, int>(ss, int.Parse);
+
+
+
+
+More0(a);
+
+
+
+
+
+void More0(int[] a)
+{
+    int k = 0;
+    for (int i = 0; i < a.Length; i++)
+        if (a[i] > 0)
+            k++;
+
+    Console.WriteLine(k);
+}
