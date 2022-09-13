8b699b599172f8bc97f60a601ae5d82fc2276f2a
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-09-13 16:38:08 +0300

    Целое число или нет

---
+++ String_5/Program.cs
@@ -0,0 +1,54 @@
+﻿// Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
+//Вычислить сумму цифр введенного числа
+
+string number = Console.ReadLine();
+int sum = 0;
+foreach (var item in number)
+{
+
+    if (item == '-')
+    {
+        continue;
+    }
+    else if (item > 0 || item < 0 || item == 0)
+    {
+
+        sum += Convert.ToInt32(item.ToString());
+    }
+
+    else Console.WriteLine("Это не является верной записью целого");
+}
+Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+/*string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    if (char.IsDigit(number[i]))
+    {
+        sum += number[i] - 48;
+    }
+    else
+    {
+        // пропуск минуса в начале, если он есть
+        if (i == 0 && number[i] == '-')
+        {
+            continue;
+        }
+        else
+        {
+            Console.WriteLine("Это не является верной записью целого");
+            return;
+        }
+    }
+}
+Console.WriteLine("Сумма цифр числа: {0}", sum);
+*/
