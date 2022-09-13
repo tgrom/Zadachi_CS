b018cda139e7cbfd795eb9da29eee72ad7d4138f
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-09-13 16:01:28 +0300

    Кол-во символов до точки

---
+++ String_3/Program.cs
@@ -0,0 +1,5 @@
+﻿// С клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки 
+using System.Linq;
+string data = "текст";
+int count = data.Split(".").First().Length;
+Console.WriteLine(count);
