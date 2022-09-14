33c4681845490c0fbd739f6c98ab8ff31026db70
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-09-14 16:11:59 +0300

    Задача 66

---
+++ HW066/Program.cs
@@ -0,0 +1,20 @@
+﻿//66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+SumNumbers(m, n);
+
+void SumNumbers(int m, int n)
+{
+
+    for (int i = m; i <= n; i++)
+        sum = sum + i;
+    System.Console.Write($"{sum} ");
+
+}
