7d7fee67aa74afdc0417b09e874e3b8286dcbfa5
Romanchenko Tatyana <romanchenko_t@bsu.edu.ru>
2022-09-14 16:06:19 +0300

    Задача 64

---
+++ HW064/Program.cs
@@ -0,0 +1,18 @@
+﻿// 64: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AllNumbers(m, n);
+
+void AllNumbers(int m, int n)
+{
+
+    for (int i = m; i <= n; i++)
+        System.Console.Write($"{i} ");
+
+}
+
